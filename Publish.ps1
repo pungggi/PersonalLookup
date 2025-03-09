d1926055565e15b48f7011346a35898fcbe5c92c
Alessandro <alessandro.pungitore@amanninformatik.ch>
2025-03-09 01:20:20 +0100

    Add Publish.ps1 script for module publishing to PSGallery

---
+++ Publish.ps1
@@ -0,0 +1,7 @@
+﻿$apiKey = ""
+Register-PSRepository -Default
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+$null = Get-PSRepository -Name PSGallery
+
+$modulePath = Join-Path $env:USERPROFILE "Documents\PowerShell\Modules\PersonalLookup"
+Publish-Module -Path $modulePath -NuGetApiKey $apiKey -Verbose
